8fa60cdd3e157f101a9809efc9a2f21ce26896be
tracer-ics2024 <tracer@njuics.org>
2024-10-31 23:07:25 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:07:25 up 1 day,  8:50,  1 user,  load average: 1.70, 1.10, 0.93

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -26,7 +26,7 @@ const char *regs[] = {
 void isa_reg_display() {
     for (int i = 0; i < 16; i++)
     {
-        printf("%s : %x", reg_name(i), gpr(i));
+        printf("%s : %x\n", reg_name(i), gpr(i));
     }
 }
 
