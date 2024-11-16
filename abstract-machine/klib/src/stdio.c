13a156e5aaffbdcf8542f9174733dcb654874821
tracer-ics2024 <tracer@njuics.org>
2024-11-16 17:13:20 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:13:20 up 7 days, 6 min,  1 user,  load average: 0.44, 1.05, 1.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -17,7 +17,57 @@ int vsprintf(char *out, const char *fmt, va_list ap)
 
 int sprintf(char *out, const char *fmt, ...)
 {
-    panic("Not implemented");
+    va_list v;
+    const char *copy = fmt;
+    char *buffer = out;
+    const char *arg1;
+    int arg2;
+
+    va_start(v, fmt);
+    while (*copy != '\0')
+    {
+        if (*copy == '%')
+        {
+            copy++;
+            switch(*copy)
+            {
+                case 's':
+                    arg1 = va_arg(v, char*);
+                    while (*arg1 != '\0')
+                    {
+                        *buffer = *arg1;
+                        buffer++;
+                        arg1++;
+                    }
+                    break;
+                case 'd':
+                    arg2 = va_arg(v, int);
+                    char num_buffer[34];
+                    int i = 0;
+
+                    if (arg2 == 0) {
+                        num_buffer[i++] = '0';
+                    } else {
+                        while (arg2 > 0) {
+                            num_buffer[i++] = (arg2 % 10) + '0';
+                            arg2 /= 10;
+                        }
+                    }
+
+                    for (int j = i - 1; j >= 0; j--) {
+                        *buffer++ = num_buffer[j];
+                    }
+                    break;
+            }
+        }
+        else 
+        {
+            *buffer = *copy;
+        }
+        copy++;
+    }
+    *buffer = '\0';
+    return 1;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...)
