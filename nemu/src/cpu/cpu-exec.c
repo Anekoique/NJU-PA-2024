660e7a8d7bd2c07b6cc9da9a15c63b0de10454db
tracer-ics2024 <tracer@njuics.org>
2024-11-17 14:33:42 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:33:42 up 7 days, 21:26,  1 user,  load average: 1.09, 1.64, 1.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -51,7 +51,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
         iring_inst_num_flag = 1;
         iring_position = 0;
     }
-    sprintf(iring_buffer[iring_position], "%s", _this->logbuf);
+    sprintf(iring_buffer[iring_position++], "%s", _this->logbuf);
 
     if (g_print_step)
     {
