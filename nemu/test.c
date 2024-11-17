e541b04861c583c7dc844ab221f2ff341406e0b3
tracer-ics2024 <tracer@njuics.org>
2024-11-17 18:25:08 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:25:08 up 8 days,  1:18,  1 user,  load average: 1.07, 0.96, 0.71

--- nemu/test.c
+++ nemu/test.c
@@ -1,23 +1,7 @@
-#include <string.h>
 #include <stdio.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-typedef __uint32_t uint32_t;
-typedef __uint64_t uint64_t;
-typedef uint32_t word_t;
-
-void set(int *a)
+int main()
 {
-#define macro(i) \
-    if (i > 1) *a = 2;  \
-    else *a = 1;
-#undef macro
-}
-
-int main() {
-    char buffer[30];
-    sprintf(buffer, "%s", "helloworld!\n");
-    printf("%s", buffer);
+    printf("helloworld");
+    return 0;
 }
