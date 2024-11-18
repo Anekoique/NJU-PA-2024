fdc1d1b9620c160b52b698a1639ceca6193f51a0
tracer-ics2024 <tracer@njuics.org>
2024-11-18 14:33:09 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:33:09 up 8 days, 21:26,  1 user,  load average: 0.55, 0.83, 1.00

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -26,7 +26,7 @@ $(BINARY):: compile_git
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
-override ARGS += --ftrace=$(IMAGE).elf
+# override ARGS += --ftrace=$(IMAGE).elf
 # override ARGS += -b
 # Command to execute NEMU
 IMG ?=
