3022ad8f60168d6f08a4fcc49af9418de9ac9bf4
tracer-ics2024 <tracer@njuics.org>
2024-11-19 01:57:26 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:57:26 up  5:39,  1 user,  load average: 1.38, 1.08, 0.93

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -143,6 +143,7 @@ static void init_ftrace(char *elf_file)
     for (i = 0; i < sym_size / sizeof(Elf32_Sym); i++)
     {
         printf("sym_table[%d] : %d\n", i, sym_table[i].st_name);
+        printf("str_table[%d] : %s\n", i, &str_table[sym_table[i].st_name]);
         if (sym_table[i].st_info == STT_FUNC)
         {
             sprintf(func_table[func_num].func_name, "%s", &str_table[sym_table[i].st_name]);
