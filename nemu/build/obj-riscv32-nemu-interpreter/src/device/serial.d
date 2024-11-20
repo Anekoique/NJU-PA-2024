103bb31372a41816b0efa8fa5640edc5213d2e13
tracer-ics2024 <tracer@njuics.org>
2024-11-20 18:38:44 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:38:44 up 1 day, 22:20,  1 user,  load average: 1.48, 1.03, 0.80

--- nemu/build/obj-riscv32-nemu-interpreter/src/device/serial.d
+++ nemu/build/obj-riscv32-nemu-interpreter/src/device/serial.d
@@ -7,17 +7,17 @@ deps_/home/stoair/NJU-PA-2024/nemu/build/obj-riscv32-nemu-interpreter/src/device
     $(wildcard include/config/has/port/io.h) \
     $(wildcard include/config/serial/port.h) \
     $(wildcard include/config/serial/mmio.h) \
-  /home/stoair/NJU-PA-2024/nemu/include/utils.h \
-    $(wildcard include/config/target/native/elf.h) \
+  /home/stoair/NJU-PA-2024/nemu/include/device/map.h \
+  /home/stoair/NJU-PA-2024/nemu/include/cpu/difftest.h \
+    $(wildcard include/config/difftest.h) \
   /home/stoair/NJU-PA-2024/nemu/include/common.h \
     $(wildcard include/config/mbase.h) \
     $(wildcard include/config/msize.h) \
     $(wildcard include/config/isa64.h) \
   /home/stoair/NJU-PA-2024/nemu/include/macro.h \
   /home/stoair/NJU-PA-2024/nemu/include/debug.h \
-  /home/stoair/NJU-PA-2024/nemu/include/device/map.h \
-  /home/stoair/NJU-PA-2024/nemu/include/cpu/difftest.h \
-    $(wildcard include/config/difftest.h) \
+  /home/stoair/NJU-PA-2024/nemu/include/utils.h \
+    $(wildcard include/config/target/native/elf.h) \
   /home/stoair/NJU-PA-2024/nemu/include/difftest-def.h \
     $(wildcard include/config/isa/x86.h) \
     $(wildcard include/config/isa/mips32.h) \
