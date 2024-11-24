ca1f3f7c0509e0dd47e1364ed1b7ebbbf56634c6
tracer-ics2024 <tracer@njuics.org>
2024-11-25 00:07:55 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:07:55 up 3 days, 13:52,  3 users,  load average: 0.98, 1.43, 1.56

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -65,7 +65,7 @@ void device_update()
             uint8_t k = event.key.keysym.scancode;
             bool is_keydown = (event.key.type == SDL_KEYDOWN);
             send_key(k, is_keydown);
-            mmio_read(CONFIG_I8042_DATA_MMIO, 4);
+            //mmio_read(CONFIG_I8042_DATA_MMIO, 4);
             break;
         }
         case SDL_KEYUP:
