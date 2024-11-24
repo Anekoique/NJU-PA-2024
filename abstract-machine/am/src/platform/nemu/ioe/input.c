63140908dc540eda924cd2f8ddfc846b4710b95d
tracer-ics2024 <tracer@njuics.org>
2024-11-24 23:42:37 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:42:37 up 3 days, 13:27,  3 users,  load average: 1.44, 1.75, 1.63

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -7,7 +7,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd)
 {
     kbd->keydown = 0;
     kbd->keycode = AM_KEY_NONE;
-    uint8_t am_scancode = inl(KBD_ADDR);
-    (am_scancode & KEYDOWN_MASK) == KEYDOWN_MASK ? kbd->keydown = 1 : 0;
+    uint32_t am_scancode = inl(KBD_ADDR);
+    (am_scancode & KEYDOWN_MASK) == KEYDOWN_MASK ? kbd->keydown = true : false;
     kbd->keycode = am_scancode & 0x7fff;
 }
