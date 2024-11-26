099bbc465fc0e5cb121e2fcca4731591c50b2804
tracer-ics2024 <tracer@njuics.org>
2024-11-26 16:42:43 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:42:43 up 5 days,  6:27,  3 users,  load average: 0.78, 0.66, 0.62

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,18 +2,22 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+#define N 32
 
 void __am_gpu_init() {
     int i;
-    int w = 0;  // TODO: get the correct width
-    int h = 0;  // TODO: get the correct height
+    int w = io_read(AM_GPU_CONFIG).width / N;  // TODO: get the correct width
+    int h = io_read(AM_GPU_CONFIG).height / N;  // TODO: get the correct width
     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
     for (i = 0; i < w * h; i ++) fb[i] = i;
     outl(SYNC_ADDR, 1);
 }
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
 {
-    *cfg = (AM_GPU_CONFIG_T){.present = true, .has_accel = false, .width = 0, .height = 0, .vmemsz = 0};
+    uint32_t code = inl(VGACTL_ADDR);
+    int height = code & 0xffff;
+    int width = (code > 16) & 0xffff;
+    *cfg = (AM_GPU_CONFIG_T){.present = true, .has_accel = false, .width = width, .height = height, .vmemsz = width * height};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
