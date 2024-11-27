f8b2a599387b6e5a2845e880bfd23a58bfbe7b14
tracer-ics2024 <tracer@njuics.org>
2024-11-27 18:55:02 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:55:02 up  4:26,  1 user,  load average: 1.94, 1.45, 1.03

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -54,7 +54,7 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl)
         if (sb > (uint8_t *)((uintptr_t)AUDIO_SBUF_ADDR + AUDIO_SBUF_SIZE))
             sb = (uint8_t *)(uintptr_t)AUDIO_SBUF_ADDR;
     }
-    printf("%x\n", (AUDIO_SBUF_ADDR + AUDIO_SBUF_SIZE));
+    printf("%p\n", ((uintptr_t)AUDIO_SBUF_ADDR + (uintptr_t)AUDIO_SBUF_SIZE));
 
     outl(AUDIO_COUNT_ADDR, count + len);
 }
