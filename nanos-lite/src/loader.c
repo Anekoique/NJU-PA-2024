ec17fae2f624fbd015c39cbc3f79f4579c7a35d7
tracer-ics2024 <tracer@njuics.org>
2024-11-30 19:49:57 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:49:57 up 3 days,  5:21,  1 user,  load average: 0.62, 0.71, 0.81

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -29,7 +29,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
     Elf_Ehdr elf_header;
     ramdisk_read(&elf_header, 0, sizeof(Elf_Ehdr));
     printf("%x\n", *(uint32_t *)(elf_header.e_ident));
-    assert(*(uint32_t *)(elf_header.e_ident) == 0x457f464c);
+    assert(*(uint32_t *)(elf_header.e_ident) == 0x464c4576);
     //assert(elf_header.e_machine == EXPECT_TYPE);
     
     size_t phdr_offset = elf_header.e_phoff;
