b1474553f825016a83b7aa7b32fe22e285ab8164
tracer-ics2024 <tracer@njuics.org>
2024-12-01 03:37:23 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:37:23 up 3 days, 13:08,  1 user,  load average: 2.28, 1.41, 1.06

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <assert.h>
@@ -66,6 +67,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
+    printf("here\n");
   _exit(SYS_write);
   return 0;
 }
