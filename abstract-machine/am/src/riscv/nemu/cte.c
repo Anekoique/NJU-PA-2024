9e2694093be7c3a1bb0cc0a7500de43724652b9c
tracer-ics2024 <tracer@njuics.org>
2024-11-30 22:06:33 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:06:33 up 3 days,  7:37,  1 user,  load average: 0.39, 0.58, 0.81

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -12,7 +12,7 @@ Context *__am_irq_handle(Context *c)
         Event ev = {0};
         switch (c->mcause)
         {
-        case 11:
+        case 0:
             ev.event = EVENT_YIELD;
             c->mepc += 4;
             break;
