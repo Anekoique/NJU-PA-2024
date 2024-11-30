943309222967428822fb4c08043151d16f24da55
tracer-ics2024 <tracer@njuics.org>
2024-11-30 18:49:27 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:49:27 up 3 days,  4:20,  1 user,  load average: 1.94, 1.43, 1.06

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,22 +1,55 @@
-#include <proc.h>
 #include <elf.h>
+#include <proc.h>
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t randisk_write(const void *buf, size_t offset, size_t len);
 
 #ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(ISA_X86__)
+#define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_RISCV32__)
+# define EXPECT_TYPE EM_ARM
+#else 
+# error Unsupported ISA
+#endif
+
+static uintptr_t loader(PCB *pcb, const char *filename)
+{
+    uintptr_t entry = 0x83000000;
+
+    Elf_Ehdr elf_header;
+    ramdisk_read(&elf_header, 0, sizeof(Elf_Ehdr));
+    assert(*(uint32_t *)(elf_header.e_ident) == 0x7f454c46);
+    assert(elf_header.e_machine == EXPECT_TYPE);
+    
+    size_t phdr_offset = elf_header.e_phoff;
+    Elf_Phdr phdr;
+    for (int i = 0; i < elf_header.e_phnum; i++)
+    {
+        ramdisk_read(&phdr, phdr_offset + i * elf_header.e_phentsize, elf_header.e_phentsize);
+        if (phdr.p_type != PT_LOAD) continue;
+
+        uint8_t *segment = (uint8_t *)malloc(phdr.p_memsz);
+        ramdisk_read(segment, phdr.p_offset, phdr.p_filesz);
+        memcpy((uint8_t *)(uintptr_t)phdr.p_vaddr, segment, phdr.p_filesz);
+        memset((uint8_t *)(uintptr_t)phdr.p_vaddr + phdr.p_filesz, 0, phdr.p_memsz - phdr.p_vaddr);    
+    }
+
+    return entry;
 }
 
-void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+void naive_uload(PCB *pcb, const char *filename)
+{
+    uintptr_t entry = loader(pcb, filename);
+    Log("Jump to entry = %p", entry);
+    ((void (*)())entry)();
 }
-
