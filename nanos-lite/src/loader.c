8b3cff99fbc6fe0c0d4abd07a875f961b3eaa0b7
tracer-ics2024 <tracer@njuics.org>
2024-11-30 19:59:02 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:59:02 up 3 days,  5:30,  1 user,  load average: 1.62, 1.43, 1.13

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -38,10 +38,10 @@ static uintptr_t loader(PCB *pcb, const char *filename)
         ramdisk_read(&phdr, phdr_offset + i * elf_header.e_phentsize, elf_header.e_phentsize);
         if (phdr.p_type != PT_LOAD) continue;
 
-        uint8_t *segment = (uint8_t *)malloc(phdr.p_memsz);
+        uint32_t *segment = (uint32_t *)malloc(phdr.p_memsz);
         ramdisk_read(segment, phdr.p_offset, phdr.p_filesz);
-        memcpy((uint32_t *)(uintptr_t)phdr.p_vaddr, segment, phdr.p_filesz);
-        memset((uint32_t *)(uintptr_t)phdr.p_vaddr + phdr.p_filesz, 0, phdr.p_memsz - phdr.p_vaddr);    
+        memcpy((uint32_t *)phdr.p_vaddr, segment, phdr.p_filesz);
+        memset((uint32_t *)phdr.p_vaddr + phdr.p_filesz, 0, phdr.p_memsz - phdr.p_vaddr);    
         printf("current\n");
     }
 
