fd798535f6b29f4c0b70ad12685c92457e641dc0
tracer-ics2024 <tracer@njuics.org>
2024-11-30 20:50:06 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:50:06 up 3 days,  6:21,  1 user,  load average: 0.56, 0.40, 0.37

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -98,6 +98,7 @@ int strncmp(const char *s1, const char *s2, size_t n)
 void *memset(void *s, int c, size_t n)
 {
     size_t i;
+    printf("%d\n", n);
     for (i = 0; i < n; i++)
     {
         ((char *)s)[i] = c;
