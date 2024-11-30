1b544f69598253a8e1d1e6fc1bb4b76715d33aab
tracer-ics2024 <tracer@njuics.org>
2024-11-30 19:10:25 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:10:25 up 3 days,  4:41,  1 user,  load average: 2.32, 1.47, 1.11

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -13,7 +13,7 @@ extern uint8_t ramdisk_end;
 size_t ramdisk_read(void *buf, size_t offset, size_t len)
 {
     assert(offset + len <= RAMDISK_SIZE);
-    printf("%d\n", ramdisk_start);
+    printf("%d\n", (uint8_t *)(uintptr_t)&ramdisk_start + 1);
     memcpy(buf, &ramdisk_start + offset, len);
     return len;
 }
