316051e001c877fb406f6bf7669ff32dc21df748
tracer-ics2024 <tracer@njuics.org>
2024-12-01 18:23:56 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:23:56 up 4 days,  3:55,  1 user,  load average: 0.29, 0.70, 0.80

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -81,10 +81,9 @@ int _write(int fd, void *buf, size_t count)
 
 void *_sbrk(intptr_t increment)
 {
-    
     intptr_t *addr = heap_end;
     addr += increment;
-    intptr_t ret = _syscall_(SYS_brk, (intptr_t)heap_start, 0, 0);
+    intptr_t ret = _syscall_(SYS_brk, (intptr_t)addr, 0, 0);
     printf("ret : %p\n", (void *)ret);
     return (void*)ret;
 }
