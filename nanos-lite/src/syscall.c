4dc1c0a8d75a1847cbedf06270d5b8ab3efaaa6d
tracer-ics2024 <tracer@njuics.org>
2024-12-01 16:48:12 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:48:12 up 4 days,  2:19,  1 user,  load average: 0.91, 1.01, 0.94

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -24,8 +24,7 @@ int write(int fd, uint8_t *buf, size_t count)
 
 uintptr_t sbrk(intptr_t increment)
 {
-    int a = 1;
-    printf("%x\n", a);
+    printf("%x\n", 256);
     printf("_end : %x\n", &_end);
     char *pre = &_end;
     char *ptr = &_end;
