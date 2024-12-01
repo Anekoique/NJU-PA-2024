5ccf42b7a7a5db97b68493f1a6d0bc0530708059
tracer-ics2024 <tracer@njuics.org>
2024-12-01 14:55:28 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:28 up 4 days, 26 min,  1 user,  load average: 1.17, 1.80, 1.47

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,21 @@
 #include "syscall.h"
 #include <common.h>
+
+int write(int fd, uint32_t *buf, size_t count)
+{
+    int num = 0;
+    if (fd == 0 || fd == 1)
+    {
+        for (int i = 0; i < count && buf[i] != '\0'; i++)
+        {
+            putch(buf[i]);
+            num++;
+        }
+    }
+
+    return num;
+}
+
 void do_syscall(Context *c)
 {
     uintptr_t a[4];
@@ -10,17 +26,18 @@ void do_syscall(Context *c)
 #ifdef STRACE
     printf("syscall\ntype : %s\n", syscall_type[a[0]]);
     printf("a0 : %d, a1 : %d, a2 : %d, a3 : %d\n", a[1], a[2], a[3]);
-    printf("mcause : %d, mepc : %d\n", c->mcause, c->mepc);
 #endif
     switch (a[0])
     {
+    case 0:
+        halt(a[1]);
+        break;
     case 1:
         yield();
         c->GPRx = 0;
         break;
-    case 0:
-        printf("here\n");
-        halt(a[1]);
+    case 4:
+        c->GPRx = write((int)a[0], (uint32_t *)a[1], (size_t)a[2]);
         break;
     default:
         panic("Unhandled syscall ID = %d", a[0]);
