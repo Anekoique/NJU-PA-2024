ebc5c562fe6aa3e492d31cadbd661bd15ab4592e
tracer-ics2024 <tracer@njuics.org>
2024-12-04 17:38:00 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:38:00 up 1 day, 19:05,  1 user,  load average: 1.24, 1.32, 1.11

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -94,6 +94,7 @@ size_t fs_write(int fd, const void *buf, size_t len)
     }
     uint8_t *pos = &ramdisk_start + file_table[fd].disk_offset + file_table[fd].open_offset;
     memcpy(pos, buf, len);
+    file_table[fd].open_offset += len;
     return len;
 }
 
