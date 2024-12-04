f8d84c44692b6b9064b8b4c28864c2a9b51d50f6
tracer-ics2024 <tracer@njuics.org>
2024-12-04 16:27:00 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:27:00 up 1 day, 17:54,  1 user,  load average: 4.07, 1.88, 1.37

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -65,8 +65,9 @@ size_t fs_read(int fd, void *buf, size_t len)
     if (file_table[fd].open_offset + len > file_table[fd].size)
     {
         len = file_table[fd].size - file_table[fd].open_offset;
+        printf("Con't Reach here\n");
     }
-    uint8_t *pos = &ramdisk_start + file_table[fd].disk_offset + file_table[fd].open_offset - 1;
+    uint8_t *pos = &ramdisk_start + file_table[fd].disk_offset + file_table[fd].open_offset;
     memcpy(buf, pos, len);
     file_table[fd].open_offset += len;
     return len;
@@ -78,7 +79,7 @@ size_t fs_write(int fd, const void *buf, size_t len)
     {
         len = file_table[fd].size - file_table[fd].open_offset;
     }
-    uint8_t *pos = &ramdisk_start + file_table[fd].disk_offset + file_table[fd].open_offset - 1;
+    uint8_t *pos = &ramdisk_start + file_table[fd].disk_offset + file_table[fd].open_offset;
     memcpy(pos, buf, len);
     return len;
 }
