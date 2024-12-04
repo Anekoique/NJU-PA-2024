8635158eec29e474e8d964623f73e4d1f27afd54
tracer-ics2024 <tracer@njuics.org>
2024-12-04 17:16:14 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:16:14 up 1 day, 18:43,  1 user,  load average: 0.55, 0.77, 0.92

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,7 +7,7 @@ size_t fs_write(int fd, const void *buf, size_t len);
 size_t fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
 
-#define STRACE
+//#define STRACE
 #ifdef STRACE
 const char* syscall_names[] = {
     "SYS_exit",
