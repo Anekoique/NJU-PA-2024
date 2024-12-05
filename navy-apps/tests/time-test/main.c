7daeb67d8d293bd4a83363432bd4c82a49f83365
tracer-ics2024 <tracer@njuics.org>
2024-12-05 17:22:03 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:22:03 up 2 days, 18:49,  1 user,  load average: 0.39, 0.84, 1.02

--- navy-apps/tests/time-test/main.c
+++ navy-apps/tests/time-test/main.c
@@ -8,13 +8,13 @@ int main()
     long int last;
     gettimeofday(&now, NULL);
     last = now.tv_sec * 100000 + now.tv_usec;
-    printf("time : %d\n", now.tv_sec * 100000 + now.tv_usec);
+    printf("time : %d\n", now.tv_sec * 1000000 + now.tv_usec);
     int count = 0;
     while (count < 10)
     {
         gettimeofday(&now, NULL);
-        current = now.tv_sec * 100000 + now.tv_usec;
-        if (current - last > 500000)
+        current = now.tv_sec * 1000000 + now.tv_usec;
+        if (current - last > 5000000)
         {
             last = current;
             printf("after 5 s\n");
