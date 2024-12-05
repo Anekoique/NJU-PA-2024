1ba09b1ca80d00572190ac2fc9e8d91eb9c3559a
tracer-ics2024 <tracer@njuics.org>
2024-12-05 17:05:10 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:05:10 up 2 days, 18:32,  1 user,  load average: 1.27, 1.20, 1.18

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -14,7 +14,7 @@ static const char *keyname[256] __attribute__((used)) = {[AM_KEY_NONE] = "NONE",
 int gettimeofday(struct timeval *tv, intptr_t tz)
 {
     long int now = io_read(AM_TIMER_UPTIME).us;
-    printf("time : %ld\n", now);
+    printf("time : %d\n", now);
     tv->tv_sec = now / 1000000;
     tv->tv_uesc = now % 1000000;
     return 0;
