e23d978326ef16f8e5f4cb408d68db41ac505e16
tracer-ics2024 <tracer@njuics.org>
2024-12-06 23:24:12 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:24:12 up 4 days, 51 min,  1 user,  load average: 1.34, 1.06, 1.17

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -35,9 +35,9 @@ void NDL_OpenCanvas(int *w, int *h)
 {
     FILE *fp = fopen("/proc/dispinfo", "r+");
     char line[64];
-while (fgets(line, sizeof(line), fp)) {
-    printf("%s", line); // 检查文件内容
-}
+    while (fgets(line, sizeof(line), fp)) {
+        printf("%s", line); // 检查文件内容
+    }
     *w = screen_w;
     *h = screen_h;
     if (getenv("NWM_APP"))
