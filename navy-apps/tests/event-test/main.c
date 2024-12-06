d179556e6a2335662b2482c75746907b591c1c2f
tracer-ics2024 <tracer@njuics.org>
2024-12-06 17:26:46 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:26:46 up 3 days, 18:54,  1 user,  load average: 0.92, 1.41, 1.37

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -4,10 +4,10 @@
 int main()
 {
     NDL_Init(0);
-    printf("here\n");
     while (1)
     {
         char buf[64];
+        printf("here\n");
         if (NDL_PollEvent(buf, sizeof(buf)))
         {
             printf("receive event: %s\n", buf);
