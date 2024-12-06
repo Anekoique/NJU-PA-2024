514407840634762a38e899dfd6c90015984523be
tracer-ics2024 <tracer@njuics.org>
2024-12-06 17:28:05 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:28:05 up 3 days, 18:55,  1 user,  load average: 2.08, 1.58, 1.43

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -24,7 +24,7 @@ uint32_t NDL_GetTicks()
 
 int NDL_PollEvent(char *buf, int len)
 {
-    FILE *fp = fopen("/dev/events", 0);
+    FILE *fp = fopen("/dev/events", "r+");
     return fscanf(fp, "%s", buf);
 }
 
