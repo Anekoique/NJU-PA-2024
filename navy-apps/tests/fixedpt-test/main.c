b7da59187d4660a79920d9ec2cd52fc39b9c07fb
tracer-ics2024 <tracer@njuics.org>
2024-12-07 23:40:55 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:40:55 up 5 days,  1:08,  1 user,  load average: 1.15, 1.43, 1.06

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    fixedpt a = fixedpt_rconst(1.2);
+    fixedpt a = fixedpt_rconst(-1.2);
     fixedpt c = fixedpt_ceil(a);
     //int d = fixedpt_toint(c);
     printf("%d\n", c);
