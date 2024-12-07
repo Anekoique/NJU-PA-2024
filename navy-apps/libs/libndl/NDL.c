4b55b0c892a502b1b04f37712dd1e96c0bd1d3e5
tracer-ics2024 <tracer@njuics.org>
2024-12-07 17:35:39 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:35:39 up 4 days, 19:02,  1 user,  load average: 0.98, 1.06, 0.91

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -75,7 +75,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h)
     for (int i = 0; i < h; i++)
     {
         fseek(fp, (y + i) * screen_w + x, SEEK_SET);
-        fwrite(pixels + i * w, sizeof(uint32_t), w, fp);
+        fwrite(pixels + i * w, 2, w, fp);
     }
 }
 
