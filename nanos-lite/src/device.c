2a449869e7050f1c9081c89571ec337e5794f045
tracer-ics2024 <tracer@njuics.org>
2024-12-07 14:38:17 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:38:17 up 4 days, 16:05,  1 user,  load average: 1.87, 1.66, 1.40

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -56,7 +56,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len)
 
     i ++;
     printf("%d\n", i);
-    //serial_write(buf, 0, len);
+    serial_write(buf, 0, len);
     return writed;
 }
 
