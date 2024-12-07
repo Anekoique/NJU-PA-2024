b2f1631661ca53d65ad2a6335e629be4f1da8690
tracer-ics2024 <tracer@njuics.org>
2024-12-07 14:35:22 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:35:22 up 4 days, 16:02,  1 user,  load average: 1.23, 1.38, 1.28

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -37,11 +37,11 @@ void NDL_OpenCanvas(int *w, int *h)
     char line1[64];
     char line2[64];
     fgets(line1, sizeof(line1), fp);
-    printf("%s", line1);
-    //for (int i = 0; i < 64; i++)
-    //{
-    //    printf("%c", line1[i]);
-    //}
+    //printf("%s", line1);
+    for (int i = 0; i < 64; i++)
+    {
+        printf("%c", line1[i]);
+    }
     //printf("\ncurrent\n");
     //printf("***************\n");
     //printf("***************\n");
