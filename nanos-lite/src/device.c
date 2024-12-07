d1edea60702c2cb572b0e051ede808e48b688bd7
tracer-ics2024 <tracer@njuics.org>
2024-12-07 14:53:27 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:53:27 up 4 days, 16:20,  1 user,  load average: 1.25, 1.23, 1.28

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -47,7 +47,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len)
     int w = io_read(AM_GPU_CONFIG).width;
     int h = io_read(AM_GPU_CONFIG).height;
     int writed = 20;
-    writed = snprintf(buf, len, "WIDTH : %d\nHEIGHT : %d\n\n", w, h);
+    writed = snprintf(buf, len, "WIDTH : %d\nHEIGHT : %d\n\n\n\n\n", w, h);
     serial_write(buf, 0, len);
     return writed;
 }
