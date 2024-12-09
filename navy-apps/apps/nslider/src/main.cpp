c3a9953c360a702197ed20a770df56189d63022d
tracer-ics2024 <tracer@njuics.org>
2024-12-09 23:05:54 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:05:54 up 10:40,  1 user,  load average: 1.99, 2.39, 2.24

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -28,10 +28,10 @@ void render() {
   SDL_Surface *slide = SDL_LoadBMP(fname);
   assert(slide);
   SDL_BlitSurface(slide, NULL, screen, NULL);
-  SDL_UpdateRect(screen, 0, 0, 0, 0);
   char buffer[64];
   NDL_PollEvent(buffer, sizeof(buffer));
   printf("here\n");
+  SDL_UpdateRect(screen, 0, 0, 0, 0);
   SDL_FreeSurface(slide);
 }
 
