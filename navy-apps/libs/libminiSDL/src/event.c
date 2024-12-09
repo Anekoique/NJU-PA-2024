a0a7e123c91c9fb8d009464a2d5c6e007fdab966
tracer-ics2024 <tracer@njuics.org>
2024-12-09 19:02:12 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:02:12 up  6:36,  1 user,  load average: 1.52, 1.79, 1.86

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,6 +1,7 @@
 #include "debug.h"
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -20,8 +21,31 @@ int SDL_PollEvent(SDL_Event *ev)
 
 int SDL_WaitEvent(SDL_Event *event)
 {
-    panic("Please implement me!");
-    return 1;
+    while (1)
+    {
+        char buf[64];
+        if (NDL_PollEvent(buf, sizeof(buf)) == 0) continue;
+        else 
+        {
+            char type;
+            char *name;
+            sscanf(buf, "k%c AM_%s", &type, name);
+            
+            if (type == 'u') event->key.type = SDL_KEYUP;
+            else if (type == 'd') event->key.type = SDL_KEYDOWN;
+            
+            for (int i = 0; i < sizeof(keyname) / sizeof(keyname[0]); i++)
+            {
+                if (strcmp(name, keyname[i]) == 0)
+                {
+                    event->key.keysym.sym = i;
+                    return 1;
+                }
+            }
+
+            panic("con't reach hear\n");
+        }
+    }
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask)
