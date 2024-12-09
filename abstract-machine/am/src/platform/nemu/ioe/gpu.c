233e76f802ac00269d6cbcfd967e5101bb9c5f43
tracer-ics2024 <tracer@njuics.org>
2024-12-10 00:26:58 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:26:58 up 12:01,  1 user,  load average: 1.74, 2.00, 1.92

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -35,7 +35,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
             for (int x = ctl->x; x < ctl->w + ctl->x; x++)
             {
                 fb[y * width + x] = ((uint32_t *)ctl->pixels)[i++];
-                //printf("%p\n", fb + y*width + x);
+                printf("%p\n", fb + y*width + x);
             }
         }
     }
