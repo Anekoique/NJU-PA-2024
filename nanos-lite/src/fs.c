376d3c225f6b8ef14b29a11cd719a9ca97f8e94a
tracer-ics2024 <tracer@njuics.org>
2024-12-09 22:28:49 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:28:49 up 10:03,  1 user,  load average: 1.37, 1.82, 2.04

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -76,6 +76,7 @@ int fs_open(const char *pathname, int flags, int mode)
 
 size_t fs_read(int fd, void *buf, size_t len)
 {
+    
     if (fd == 3) return events_read(buf, 0, len);
     if (fd == 4) return dispinfo_read(buf, 0, len);
     if (file_table[fd].open_offset + len > file_table[fd].size)
@@ -87,7 +88,7 @@ size_t fs_read(int fd, void *buf, size_t len)
     printf("pos : %p, disk_offset : %p, len : %d\n", pos, file_table[fd].disk_offset, len);
     //printf("fd : %d, open_offset : %d\n", fd, file_table[fd].open_offset);
     memcpy(buf, pos, len);
-    for (int i = 0; i < 64; i++)
+    for (int i = 0; i < 1; i++)
         printf("%d", pos[i]);
     printf("\n");
     file_table[fd].open_offset += len;
