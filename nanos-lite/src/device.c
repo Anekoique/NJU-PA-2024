94fee85e02fd89f188b04d0b98378bfeb09c01d5
tracer-ics2024 <tracer@njuics.org>
2024-12-10 00:59:41 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:59:41 up 12:34,  1 user,  load average: 0.45, 0.91, 1.12

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -65,7 +65,6 @@ size_t fb_write(const void *buf, size_t offset, size_t len)
     printf("buffer position : %p\n", buf);
     io_write(AM_GPU_FBDRAW, x, y, buffer, len / sizeof(uint32_t), 1, false);
     io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
-    return 0;
     if (*(uint8_t *)check_pos != 66 && check_flag) 
     {
         printf("check_pos : %p\n", check_pos);
@@ -73,6 +72,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len)
         printf("error\n");
         panic("error");
     }
+    return 0;
 }
 
 void init_fb(int *w, int *h)
