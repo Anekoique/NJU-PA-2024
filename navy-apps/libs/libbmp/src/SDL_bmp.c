4147c38cd6ede37c8b9d2f1843003aecf22a3b64
tracer-ics2024 <tracer@njuics.org>
2024-12-09 19:57:50 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:57:50 up  7:32,  1 user,  load average: 5.47, 2.93, 2.41

--- navy-apps/libs/libbmp/src/SDL_bmp.c
+++ navy-apps/libs/libbmp/src/SDL_bmp.c
@@ -6,9 +6,9 @@ SDL_Surface* SDL_LoadBMP(const char *filename) {
   int w, h;
   void *pixels = BMP_Load(filename, &w, &h);
   assert(pixels);
+  printf("success\n");
   SDL_Surface *s = SDL_CreateRGBSurfaceFrom(pixels, w, h, 32, w * sizeof(uint32_t),
       DEFAULT_RMASK, DEFAULT_GMASK, DEFAULT_BMASK, DEFAULT_AMASK);
-  printf("success\n");
   s->flags &= ~SDL_PREALLOC;
   return s;
 }
