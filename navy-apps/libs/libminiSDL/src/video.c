04127f04b2c9ec13f5e5a01597f89d5a05043fb2
tracer-ics2024 <tracer@njuics.org>
2024-12-09 23:12:30 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:12:30 up 10:47,  1 user,  load average: 0.57, 1.09, 1.69

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -34,13 +34,13 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h)
     width = s->w;
     height = s->h;
     NDL_OpenCanvas(&width, &height);
-    char buffer[64];
-    NDL_PollEvent(buffer, sizeof(buffer));
-    printf("here\n");
 
     if (w == 0 && h == 0)
         NDL_DrawRect((uint32_t *)s->pixels, x, y, width, height);
     else NDL_DrawRect((uint32_t *)s->pixels, x, y, w, h);
+    char buffer[64];
+    NDL_PollEvent(buffer, sizeof(buffer));
+    printf("here\n");
 }
 
 // APIs below are already implemented.
