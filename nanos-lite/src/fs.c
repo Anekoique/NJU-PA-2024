8724b80f37519066fb0fb22b15db2262eb76c132
tracer-ics2024 <tracer@njuics.org>
2024-12-09 23:23:02 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:23:02 up 10:57,  1 user,  load average: 5.13, 2.55, 1.92

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -12,8 +12,8 @@ size_t dispinfo_read(void *buf, size_t ooffset, size_t len);
 size_t fb_write(const void *buf, size_t offset, size_t len);
 void init_fb(int *w, int *h);
 
-static intptr_t check_pos = 0x80064a37;
-bool check_flag = false;
+//static intptr_t check_pos = 0x80064a37;
+//bool check_flag = false;
 
 typedef struct
 {
@@ -79,12 +79,12 @@ int fs_open(const char *pathname, int flags, int mode)
 
 size_t fs_read(int fd, void *buf, size_t len)
 {
-    if (*(uint8_t *)check_pos != 66 && check_flag) 
-    {
-        printf("check_pos : %d", *(uint8_t *)check_pos);
-        printf("error");
-        panic("error");
-    }
+    //if (*(uint8_t *)check_pos != 66 && check_flag) 
+    //{
+    //    printf("check_pos : %d", *(uint8_t *)check_pos);
+    //    printf("error");
+    //    panic("error");
+    //}
     if (fd == 3) return events_read(buf, 0, len);
     if (fd == 4) return dispinfo_read(buf, 0, len);
     if (file_table[fd].open_offset + len > file_table[fd].size)
@@ -93,16 +93,15 @@ size_t fs_read(int fd, void *buf, size_t len)
         printf("Con't Reach here\n");
     }
     uint8_t *pos = &ramdisk_start + file_table[fd].disk_offset + file_table[fd].open_offset;
-    printf("pos : %p, disk_offset : %p, len : %d\n", pos, file_table[fd].disk_offset, len);
     memcpy(buf, pos, len);
-    for (int i = 0; i < 1; i++)
-        printf("%d", pos[i]);
-    printf("\n");
-    if (file_table[fd].disk_offset == 0x61b0f && len == 1024 && !check_flag)
-    {
-        check_flag = true;
-        check_pos = (intptr_t)pos;
-    }
+    //for (int i = 0; i < 1; i++)
+    //    printf("%d", pos[i]);
+    //printf("\n");
+    //if (file_table[fd].disk_offset == 0x61b0f && len == 1024 && !check_flag)
+    //{
+    //    check_flag = true;
+    //    check_pos = (intptr_t)pos;
+    //}
     file_table[fd].open_offset += len;
     return len;
 }
