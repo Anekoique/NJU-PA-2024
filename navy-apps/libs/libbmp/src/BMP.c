cccac40ed2e45e2729e0592c952430a6defde94e
tracer-ics2024 <tracer@njuics.org>
2024-12-09 21:06:28 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:06:28 up  8:41,  1 user,  load average: 2.27, 2.99, 2.86

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -28,7 +28,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
 
   //if (hdr.bitcount != 24 && hdr.bitcount != 32) return NULL;
-  if (hdr.compression != 0) return NULL;
+  //if (hdr.compression != 0) return NULL;
   printf("here\n");
   int w = hdr.width;
   int h = hdr.height;
