3277e6f7f66bd9e39af82d5b60b82708afcc0251
tracer-ics2024 <tracer@njuics.org>
2024-12-09 19:53:20 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:53:20 up  7:27,  1 user,  load average: 2.53, 2.12, 2.17

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -51,12 +51,11 @@ int main() {
 
   int rep = 0, g = 0;
 
-  //render();
+  render();
 
   while (1) {
     SDL_Event e;
     SDL_WaitEvent(&e);
-    render();
 
     if (e.type == SDL_KEYDOWN) {
       switch(e.key.keysym.sym) {
