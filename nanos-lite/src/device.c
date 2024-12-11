fc9c7bf2123f02c1ade8c0e2a35e4e8a23881542
tracer-ics2024 <tracer@njuics.org>
2024-12-11 15:52:34 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:52:34 up  7:49,  1 user,  load average: 0.85, 1.44, 1.69

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -61,17 +61,17 @@ size_t fb_write(const void *buf, size_t offset, size_t len)
     uint32_t *buffer = (uint32_t *)buf;
     int x = offset / sizeof(uint32_t) % 400;
     int y = offset / sizeof(uint32_t) / 400;
-    printf("x : %d, y : %d, len : %d\n", x, y, len);
-    printf("buffer position : %p\n", buf);
+    //printf("x : %d, y : %d, len : %d\n", x, y, len);
+    //printf("buffer position : %p\n", buf);
     io_write(AM_GPU_FBDRAW, x, y, buffer, len / sizeof(uint32_t), 1, false);
     io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
-    if (*(uint8_t *)check_pos != 66 && check_flag) 
-    {
-        printf("check_pos : %p\n", check_pos);
-        printf("check_pos : %d\n", *(uint8_t *)check_pos);
-        printf("error\n");
-        panic("error");
-    }
+    //if (*(uint8_t *)check_pos != 66 && check_flag) 
+    //{
+    //    printf("check_pos : %p\n", check_pos);
+    //    printf("check_pos : %d\n", *(uint8_t *)check_pos);
+    //    printf("error\n");
+    //    panic("error");
+    //}
     return len;
 }
 
