cd83c06c05332a2c8bca508f1af91a090447e9ba
tracer-ics2024 <tracer@njuics.org>
2024-12-12 19:30:14 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:30:14 up 21:12,  1 user,  load average: 4.33, 4.91, 4.53

--- navy-apps/libs/libSDL_image/src/SDL_stbimage.h
+++ navy-apps/libs/libSDL_image/src/SDL_stbimage.h
@@ -210,7 +210,6 @@ SDL_STBIMG_DEF SDL_Surface* STBIMG_LoadFromMemory(const unsigned char* buffer, i
 		SDL_SetError("STBIMG_LoadFromMemory(): Couldn't get image info: %s!\n", stbi_failure_reason());
 		return NULL;
 	}
-    printf("current\n");
 
 	// no alpha => use RGB, else use RGBA
 	origin_has_alpha = !(img.format == STBI_grey || img.format == STBI_rgb);
@@ -223,6 +222,7 @@ SDL_STBIMG_DEF SDL_Surface* STBIMG_LoadFromMemory(const unsigned char* buffer, i
 		return NULL;
 	}
 	img.format = bppToUse;
+    printf("current\n");
 
 	ret = STBIMG__CreateSurfaceImpl(img, origin_has_alpha, 1);
 
