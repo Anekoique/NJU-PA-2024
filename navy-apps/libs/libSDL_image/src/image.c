a6203b10c657535ab4acd583702cf2d5b22dc59d
tracer-ics2024 <tracer@njuics.org>
2024-12-12 19:15:25 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:15:25 up 20:57,  1 user,  load average: 2.41, 2.95, 3.82

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -19,7 +19,7 @@ SDL_Surface *IMG_Load(const char *filename)
     size_t size = lseek(fd, 0, SEEK_END);
     printf("size : %d\n", size);
     void * buf = (void *)SDL_malloc(size);
-
+    printf("here");
     lseek(fd, 0, SEEK_SET);
     read(fd, buf, size);
     SDL_Surface *ptr = STBIMG_LoadFromMemory((unsigned char *)buf, size);
