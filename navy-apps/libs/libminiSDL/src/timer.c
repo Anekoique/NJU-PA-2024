f828517e5d5a24ad0e94f97636af44268c810958
tracer-ics2024 <tracer@njuics.org>
2024-12-12 17:52:51 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:52:51 up 19:35,  1 user,  load average: 7.38, 4.67, 3.63

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -25,5 +25,12 @@ uint32_t SDL_GetTicks()
 
 void SDL_Delay(uint32_t ms)
 {
+    uint32_t start = SDL_GetTicks();
+    while (1)
+    {
+        uint32_t current = SDL_GetTicks();
+        if (current - start > ms)
+            return;
+    }
     //panic("Please implement me!");
 }
