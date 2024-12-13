c3f01e2b7abbb5ac749faff165929b33b7a7d16e
tracer-ics2024 <tracer@njuics.org>
2024-12-13 22:16:02 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:16:02 up 1 day, 23:58,  1 user,  load average: 3.72, 1.86, 1.08

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -10,17 +10,17 @@ int SDL_OpenAudio(SDL_AudioSpec *desired, SDL_AudioSpec *obtained)
 
 void SDL_CloseAudio()
 {
-    panic("Please implement me!");
+    //panic("Please implement me!");
 }
 
 void SDL_PauseAudio(int pause_on)
 {
-    panic("Please implement me!");
+    //panic("Please implement me!");
 }
 
 void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume)
 {
-    panic("Please implement me!");
+    //panic("Please implement me!");
 }
 
 SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audio_buf, uint32_t *audio_len)
@@ -31,15 +31,15 @@ SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audi
 
 void SDL_FreeWAV(uint8_t *audio_buf)
 {
-    panic("Please implement me!");
+    //panic("Please implement me!");
 }
 
 void SDL_LockAudio()
 {
-    panic("Please implement me!");
+    //panic("Please implement me!");
 }
 
 void SDL_UnlockAudio()
 {
-    panic("Please implement me!");
+    //panic("Please implement me!");
 }
