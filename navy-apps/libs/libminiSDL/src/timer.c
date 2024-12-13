e4285a173a6a33336387f4e83ca7fe791a533e82
tracer-ics2024 <tracer@njuics.org>
2024-12-14 01:56:37 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:56:37 up 2 days,  3:38,  1 user,  load average: 0.45, 1.06, 1.43

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -18,8 +18,8 @@ int SDL_RemoveTimer(SDL_TimerID id)
 uint32_t SDL_GetTicks()
 {
     static uint32_t start_time = 0;
-    if (!start_time) start_time = NDL_GetTicks();
-    uint32_t current = NDL_GetTicks();
+    if (!start_time) start_time = NDL_GetTicks() / 1000;
+    uint32_t current = NDL_GetTicks() / 1000;
     printf("return : %d\n", current-start_time);
     return current - start_time;
 }
