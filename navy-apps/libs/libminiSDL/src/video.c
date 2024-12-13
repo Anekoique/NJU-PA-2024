0026ebb5a9828510d97176b3aafdcbff3dddb337
tracer-ics2024 <tracer@njuics.org>
2024-12-14 00:31:15 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:31:15 up 2 days,  2:13,  1 user,  load average: 1.98, 1.86, 1.47

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 static int i = 0;
 
 static int maskToShift(uint32_t);
@@ -68,6 +69,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 }
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color)
 {
+    printf("get here ? \n");
     int x, y;
     uint16_t w, h;
     if (dstrect == NULL) 
