449e77fde87ce060306f384412ca4d63b4c2ca55
tracer-ics2024 <tracer@njuics.org>
2024-12-14 20:26:18 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:26:18 up  2:23,  1 user,  load average: 2.22, 2.44, 2.29

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -158,6 +158,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h)
             }
         }
 
+        printf("width : %d, pitch : %d\n", width, pitch);
         NDL_DrawRect(pixels, x, y, width, height);
         free(pixels);
         return;
