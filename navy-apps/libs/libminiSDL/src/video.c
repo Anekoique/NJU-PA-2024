0b0806021354a166c697b861232a3213e54575cd
tracer-ics2024 <tracer@njuics.org>
2024-12-14 23:11:13 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:11:13 up  5:08,  1 user,  load average: 2.30, 1.53, 1.94

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -51,10 +51,15 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
         {
             for (int j = 0; j < w; j++)
             {
-                ((uint32_t *)(dst->pixels))[(i + y) * screen_w + j + x] = 
-                    ((uint32_t *)(src->pixels))[(i + src_y) * src_w + j + src_x];
+                printf("1 : %d\n", (i + y) * screen_w + j + x);
+                printf("2 : %d\n", (i + src_y) * src_w + j + src_x);
+
+                ((uint8_t *)(dst->pixels))[(i + y) * screen_w + j + x] = 
+                    ((uint8_t *)(src->pixels))[(i + src_y) * src_w + j + src_x];
             }
         }
+
+        
         return;
     }
 
