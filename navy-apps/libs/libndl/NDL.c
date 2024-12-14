6bf73e541377e484bd4514460322644ad89c271d
tracer-ics2024 <tracer@njuics.org>
2024-12-14 23:56:31 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:56:31 up  5:53,  1 user,  load average: 2.24, 2.34, 2.14

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -82,12 +82,12 @@ void NDL_OpenCanvas(int *w, int *h)
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h)
 {
-    int mid_x = screen_w / 2, mid_y = screen_h / 2;
-    if (x + w < screen_w && x + h < screen_h)
-    {
-        x = mid_x - w / 2;
-        y = mid_y - h / 2;
-    }
+    //int mid_x = screen_w / 2, mid_y = screen_h / 2;
+    //if (x + w < screen_w && x + h < screen_h)
+    //{
+    //    x = mid_x - w / 2;
+    //    y = mid_y - h / 2;
+    //}
 
     //int fp = open("/dev/fb", 0);
     //for (int i = 0; i < h; i++)
