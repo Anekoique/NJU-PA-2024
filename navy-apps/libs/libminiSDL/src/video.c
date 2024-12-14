53c01cee2f4a175b94dae5d47ea796e9ff166f5c
tracer-ics2024 <tracer@njuics.org>
2024-12-14 20:16:16 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:16:16 up  2:13,  1 user,  load average: 1.59, 1.56, 1.91

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -123,59 +123,48 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color)
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h)
 {
-    // Determine the width and height of the update region
-    int width = (w == 0) ? s->w : w;
-    int height = (h == 0) ? s->h : h;
 
-    // Ensure canvas dimensions match the update region
+    int width, height;
+    width = w == 0 ? s->w : w;
+    height = h == 0 ? s->h : h;
     NDL_OpenCanvas(&width, &height);
-
-    uint16_t pitch = s->pitch; // Number of bytes per row
+    uint16_t pitch = s->pitch;
 
     if (s->format->BitsPerPixel == 8)
     {
-        // Allocate a temporary buffer for converted pixel data
-        uint32_t *pixels = (uint32_t *)malloc(sizeof(uint32_t) * width * height);
-        if (pixels == NULL)
-        {
-            fprintf(stderr, "Memory allocation failed\n");
-            return;
-        }
 
-        // Extract palette information
+        uint32_t *pixels;
+        pixels = (uint32_t *)malloc(sizeof(uint32_t) * width * height);
         SDL_Color *colors = s->format->palette->colors;
 
-        // Define the target pixel format (RGBA8888)
-        SDL_PixelFormat fmt = {
-            .BytesPerPixel = 4,
-            .Rshift = maskToShift(0x00ff0000),
-            .Gshift = maskToShift(0x0000ff00),
-            .Bshift = maskToShift(0x000000ff),
-            .Ashift = maskToShift(0xff000000),
-        };
+        uint8_t r;
+        uint8_t g;
+        uint8_t b;
 
-        // Convert 8-bit indexed pixels to 32-bit RGBA pixels
+        SDL_PixelFormat fmt;
+        fmt.BytesPerPixel = 4;
+        fmt.Rshift = maskToShift(0x00ff0000);
+        fmt.Gshift = maskToShift(0x0000ff00);
+        fmt.Bshift = maskToShift(0x000000ff);
+
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
-                uint8_t index = s->pixels[i * pitch + j];
-                SDL_Color color = colors[index];
-                pixels[i * width + j] = SDL_MapRGBA(&fmt, color.r, color.g, color.b, 255);
+                r = colors[s->pixels[i * width + j]].r;
+                g = colors[s->pixels[i * width + j]].g;
+                b = colors[s->pixels[i * width + j]].b;
+                pixels[i * pitch + j] = SDL_MapRGBA(&fmt, r, g, b, 0);
             }
         }
 
-        // Draw the converted pixels to the canvas
         NDL_DrawRect(pixels, x, y, width, height);
-
-        // Free the temporary buffer
         free(pixels);
+        return;
     }
-    else
-    {
-        // Handle other pixel formats (e.g., 16-bit or 32-bit)
-        NDL_DrawRect((uint32_t *)s->pixels, x, y, width, height);
-    }
+
+    printf("can not get here\n");
+    NDL_DrawRect((uint32_t *)s->pixels, x, y, width, height);
 }
 // APIs below are already implemented.
 
