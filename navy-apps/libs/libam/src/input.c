8527d156e0a4202ae8157d0ee268b76ecd667d7e
tracer-ics2024 <tracer@njuics.org>
2024-12-17 21:57:25 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:57:25 up 3 days,  3:54,  1 user,  load average: 1.53, 1.43, 1.13

--- navy-apps/libs/libam/src/input.c
+++ navy-apps/libs/libam/src/input.c
@@ -16,8 +16,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd)
         char type, name[64];
         sscanf(buf, "k%c %s", &type, name);
         printf("here\n");
-        printf("")
-        kbd->keydown = type == 'u' ? false : true;
+        kbd->keydown = ((type == 'u') ? false : true);
         for (int i = 0; i < KEYNUM; i++)
         {
             if (strcmp(name, keyname[i]) == 0)
