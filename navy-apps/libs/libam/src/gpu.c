f2b141c615b62272dcdb94a69303e336bcb65802
tracer-ics2024 <tracer@njuics.org>
2024-12-17 16:18:58 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:18:58 up 2 days, 22:16,  1 user,  load average: 0.95, 0.89, 0.82

--- navy-apps/libs/libam/src/gpu.c
+++ navy-apps/libs/libam/src/gpu.c
@@ -19,8 +19,8 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
 {
-    NDL_OpenCanvas(ctl->w, ctl->h);
-    NDL_DrawRect(ctl->pixels, ctl->x, ctl->y, ctl->w, ctl->h);
+    NDL_OpenCanvas(&ctl->w, &ctl->h);
+    NDL_DrawRect((void *)ctl->pixels, ctl->x, ctl->y, ctl->w, ctl->h);
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status)
--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -7,11 +7,9 @@ extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 void putch(char ch)
 {
     printf("%c", ch);
-    return;
 }
 
 void halt(int code)
 {
     _syscall_(SYS_exit, code, 0, 0);
-    return;
 }
