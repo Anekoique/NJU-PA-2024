186b2fccfc3e31386d4ff24fe86e15cf6c865cdb
tracer-ics2024 <tracer@njuics.org>
2024-12-18 09:51:07 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:51:07 up  1:09,  1 user,  load average: 1.25, 1.08, 0.97

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -9,7 +9,7 @@ void init_proc(void);
 
 int main(int argc, char *argv[])
 {
-    printf("******%d*******\n", argc);
+    printf("%d*************\n", argc);
     extern const char logo[];
     printf("%s", logo);
     Log("'Hello World!' from Nanos-lite");
