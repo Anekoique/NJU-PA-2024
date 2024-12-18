550289ef9c54da67af1667b262f0ac4d6eaf34c8
tracer-ics2024 <tracer@njuics.org>
2024-12-18 22:55:32 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:55:32 up  3:04,  1 user,  load average: 1.56, 0.87, 0.95

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,7 +31,7 @@ void init_proc(char *argv[])
     Log("Initializing processes...");
 
     // load program here
-    naive_uload(NULL, "/bin/nslider", argv);
+    naive_uload(NULL, "/bin/menu", argv);
 }
 
 Context *schedule(Context *prev)
