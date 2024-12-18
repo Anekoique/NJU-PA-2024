934d5e2ba2c64cca9a6000721ec56c081c5802cf
tracer-ics2024 <tracer@njuics.org>
2024-12-18 23:24:18 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:24:18 up  3:32,  1 user,  load average: 0.38, 0.94, 1.03

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -73,7 +73,7 @@ void do_syscall(Context *c)
     a[2] = c->GPR3;
     a[3] = c->GPR4;
 #ifdef STRACE
-    printf("syscall\ntype : %s\n", syscall_names[a[0]]);
+    printf("syscall type : %s\n", syscall_names[a[0]]);
     printf("a0 : %d, a1 : %d, a2 : %d\n", a[1], a[2], a[3]);
 #endif
     switch (a[0])
