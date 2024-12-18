af3599126ae79aa14a6a56d32717b6ad0af468e8
tracer-ics2024 <tracer@njuics.org>
2024-12-18 23:43:35 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:43:35 up  3:52,  1 user,  load average: 1.33, 1.67, 1.61

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -182,7 +182,6 @@ static void display_menu(int n) {
 
   printf("========================================\n");
   printf("Please Choose.\n");
-  fflush(stdout);
   printf("gethere?\n");
   fflush(stdout);
 }
