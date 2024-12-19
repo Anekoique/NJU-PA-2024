c9898f0cc58bed541b2330721d05f9376127dd74
tracer-ics2024 <tracer@njuics.org>
2024-12-19 14:51:01 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:51:01 up  4:42,  1 user,  load average: 1.97, 1.88, 1.65

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -30,7 +30,6 @@ static void sh_execute(char *command, char **mainargs, int arg_num)
     }
     else if (strcmp(command, "echo") == 0)
     {
-        assert(arg_num = 1);
         sh_printf("%s", mainargs[0]);
     }
 }
