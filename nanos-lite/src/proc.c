f21aac3ceea2be62d79c6df300ff78cd4602818f
tracer-ics2024 <tracer@njuics.org>
2024-12-22 03:51:25 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:51:25 up  4:27,  1 user,  load average: 2.04, 2.20, 2.08

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -41,7 +41,6 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     int *ptr = (int *)((intptr_t)argv - sizeof(int));
     *ptr = argc;
     pcb->cp->GPRx = (uintptr_t)ptr;
-    printf("hello\n");
 }
 
 void init_proc()
@@ -52,6 +51,7 @@ void init_proc()
     };
     context_kload(&pcb[0], hello_fun, (void *)('a'));
     context_uload(&pcb[1], "/bin/pal", argv, NULL);
+    printf("hello\n");
 
     switch_boot_pcb();
 
