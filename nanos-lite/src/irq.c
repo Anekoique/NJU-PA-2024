df644a6409aeadfd192b57644d1fa42e0d264de9
tracer-ics2024 <tracer@njuics.org>
2024-12-22 01:13:29 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:13:29 up  1:49,  1 user,  load average: 1.69, 1.21, 1.26

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -8,6 +8,7 @@ static Context *do_event(Event e, Context *c)
     switch (e.event)
     {
     case EVENT_YIELD:
+        printf("yield\n");
         schedule(c);
         break;
     case EVENT_SYSCALL:
