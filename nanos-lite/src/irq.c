aecfd0e820f510061ecb3e79ea2523473a67e258
tracer-ics2024 <tracer@njuics.org>
2024-12-22 01:09:30 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:09:30 up  1:45,  1 user,  load average: 0.63, 1.33, 1.34

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,13 +1,14 @@
 #include <common.h>
 
 void do_syscall(Context *c);
+Context *schedule(Context *prev);
 
 static Context *do_event(Event e, Context *c)
 {
     switch (e.event)
     {
     case EVENT_YIELD:
-        printf("event yield!\n");
+        schedule(c);
         break;
     case EVENT_SYSCALL:
         do_syscall(c);
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -18,20 +18,28 @@ void hello_fun(void *arg)
     int j = 1;
     while (1)
     {
-        Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
+        Log("Hello World from Nanos-lite with arg '%c' for the %dth time!", (uintptr_t)arg, j);
         j++;
         yield();
     }
 }
 
+void context_kload(PCB *pcb, void (*entry)(void *), void *arg)
+{
+    pcb->cp = kcontext((Area){pcb->stack, &(pcb->stack[STACK_SIZE])}, entry, arg);
+}
+
 void init_proc(char *argv[])
 {
+    context_kload(&pcb[0], hello_fun, (void *)('a'));
+    context_kload(&pcb[1], hello_fun, (void *)('a'));
+
     switch_boot_pcb();
 
     Log("Initializing processes...");
 
     // load program here
-    naive_uload(NULL, "/bin/nterm", argv);
+    //naive_uload(NULL, "/bin/nterm", argv);
 }
 
 Context *schedule(Context *prev)
