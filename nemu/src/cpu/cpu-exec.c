57f958fc06689af66cc5fdaed61c2d3193fab35b
tracer-ics2024 <tracer@njuics.org>
2024-12-22 15:30:51 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:30:51 up 16:07,  1 user,  load average: 3.86, 1.76, 0.93

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -23,7 +23,7 @@
  * This is useful when you use the `si' command.
  * You can modify this value as you want.
  */
-#define MAX_INST_TO_PRINT 100010
+#define MAX_INST_TO_PRINT 100
 #ifdef CONFIG_ITRACE_COND
 //static int flag = 0;
 #endif
