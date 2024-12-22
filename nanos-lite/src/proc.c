7c74caedf55c027784aa0346cc8649ac8f61e6fa
tracer-ics2024 <tracer@njuics.org>
2024-12-22 16:52:18 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:52:18 up 17:28,  1 user,  load average: 0.95, 0.64, 0.84

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -5,6 +5,10 @@
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
+char *const argv[] = {
+    "--skip",    
+    NULL     
+};
 
 uintptr_t naive_uload(PCB *, const char *, char **);
 
@@ -51,10 +55,6 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 
 void init_proc()
 {
-    char *const argv[] = {
-        "--skip",    
-        NULL     
-    };
     context_kload(&pcb[0], hello_fun, (void *)('a'));
     context_uload(&pcb[1], "/bin/pal", argv, NULL);
     uintptr_t address = 0x8220cf98; // 假设这是你想访问的地址
