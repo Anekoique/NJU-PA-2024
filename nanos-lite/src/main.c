89eff5f79eb7ae0c750d31f791f8161a2d5f4e80
tracer-ics2024 <tracer@njuics.org>
2024-12-22 17:01:19 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:01:19 up 17:37,  1 user,  load average: 1.05, 1.58, 1.24

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,7 +30,11 @@ int main(int argc, char *argv[])
     init_proc(argv);
 
     Log("Finish initialization");
-
+    uintptr_t address = 0x8220cf98; // 假设这是你想访问的地址
+    int value;
+    value = *(int *)address;
+    printf("Value at address 0x%p: %p\n", (unsigned int)address, value);
+    printf("hello\n");
 #ifdef HAS_CTE
     yield();
 #endif
