0207a95ebe242a6b6d6587aa67900edba54d9e37
tracer-ics2024 <tracer@njuics.org>
2024-12-22 16:27:17 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:27:17 up 17:03,  1 user,  load average: 1.21, 1.24, 1.23

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -65,7 +65,10 @@ void init_proc()
 
 Context *schedule(Context *prev)
 {
-    printf("%x\n", *(int *)(0x8220cf98));
+    uintptr_t address = 0x8220cf98; // 假设这是你想访问的地址
+    int value;
+    value = *(int *)address;
+    printf("Value at address 0x%x: %x\n", (unsigned int)address, value);
     current->cp = prev;
     current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
     return current->cp;
