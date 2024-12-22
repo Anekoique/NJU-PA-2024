0db091a7bb0056f9b4628414fd58568f4a221072
tracer-ics2024 <tracer@njuics.org>
2024-12-22 15:53:35 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:53:35 up 16:29,  1 user,  load average: 1.50, 1.67, 1.54

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -10,8 +10,8 @@ void call_main(uintptr_t *args)
     int argc = *((int *)(args));
     uintptr_t argv = (uintptr_t)((int *)(args) + 1);
     uintptr_t envp = (uintptr_t)((char **)argv + argc);
-    printf("argc : %d\n", argc);
     environ = (char **)envp;
+    printf("argc : %d\n", argc);
     exit(main(argc, (char **)argv, (char **)envp));
     assert(0);
 }
