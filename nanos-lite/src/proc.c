d06ae8940b3be0e25298316d92932d5065fdcd7b
tracer-ics2024 <tracer@njuics.org>
2024-12-22 16:29:35 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:29:35 up 17:05,  1 user,  load average: 1.67, 1.32, 1.25

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -43,6 +43,10 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     pcb->cp->GPRx = (intptr_t)(&argv) - sizeof(int);
     printf("%p\n", (intptr_t)(&argv) - sizeof(int));
     printf("%x\n", *(int *)((intptr_t)(&argv) - sizeof(int)));
+    uintptr_t address = 0x8220cf98; // 假设这是你想访问的地址
+    int value;
+    value = *(int *)address;
+    printf("Value at address 0x%p: %p\n", (unsigned int)address, value);
 }
 
 void init_proc()
