3fb12298f11b7cd8b9e3dd9e0b5306fa07d60ea4
tracer-ics2024 <tracer@njuics.org>
2024-12-29 02:15:47 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:15:47 up 1 day,  4:11,  1 user,  load average: 4.32, 3.15, 2.66

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -54,8 +54,6 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 {
     printf("uload : %s\n", argv[0]);
     printf("%p\n", argv[0]);
-    uintptr_t entry = naive_uload(pcb, filename, NULL);
-    pcb->cp = ucontext(NULL, (Area){pcb->stack, &(pcb->stack[STACK_SIZE])}, (void *)entry);
     
     //implement : placed mm maloc
     int argc = 0;
@@ -87,6 +85,8 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     *v_ptr = NULL;
     pcb->cp->GPRx = (uintptr_t)c_ptr;
 
+    uintptr_t entry = naive_uload(pcb, filename, NULL);
+    pcb->cp = ucontext(NULL, (Area){pcb->stack, &(pcb->stack[STACK_SIZE])}, (void *)entry);
 
     // implement : placed heap.end
     //int argc = 0;
