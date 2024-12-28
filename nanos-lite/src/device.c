52f1f801236d741cded2b726848bed1b79340d12
tracer-ics2024 <tracer@njuics.org>
2024-12-29 00:39:41 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:39:41 up 1 day,  2:35,  1 user,  load average: 2.83, 2.50, 1.30

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,7 +25,7 @@ int gettimeofday(struct _timeval *tv, intptr_t tz)
 
 size_t serial_write(const void *buf, size_t offset, size_t len)
 {
-    yield();
+    //yield();
     int num = 0;
     for (int i = 0; i < len && *(uint8_t *)buf != '\0'; i++)
     {
@@ -38,7 +38,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len)
 
 size_t events_read(void *buf, size_t offset, size_t len)
 {
-    yield();
+    //yield();
     AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
     if (ev.keycode == AM_KEY_NONE) return 0;
     int writed = 0;
