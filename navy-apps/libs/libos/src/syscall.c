8b572964a82cee3e5ea66fd36e20be94d34e6b83
tracer-ics2024 <tracer@njuics.org>
2024-12-29 02:08:31 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:08:31 up 1 day,  4:03,  1 user,  load average: 1.69, 2.67, 2.27

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -115,6 +115,7 @@ int _gettimeofday(struct timeval *tv, struct timezone *tz)
 
 int _execve(const char *fname, char *const argv[], char *const envp[])
 {
+    printf("%s\n", argv[0]);
     int ret = _syscall_(SYS_execve, (intptr_t)fname, (intptr_t)argv, (intptr_t)envp);
     return ret;
 }
