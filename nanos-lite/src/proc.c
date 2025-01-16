d1f951a9db78b397fbe42eaf524f597ea636ffa6
tracer-ics2024 <tracer@njuics.org>
2025-01-16 16:28:31 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:28:31 up 20:44,  1 user,  load average: 0.82, 1.24, 1.20

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -73,6 +73,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     int *c_ptr = (int *)(addr - string_tab - sizeof(int) - offset);
     *c_ptr = argc;
     char **v_ptr = (char **)((intptr_t)c_ptr + sizeof(int));
+    printf("here\n");
     for (int i = 0; argv[i] != NULL; i++)
     {
         memcpy(addr, argv[i], strlen(argv[i]) + 1);
@@ -80,6 +81,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
         addr += strlen(argv[i]) + 1;
         v_ptr += 1;
     }
+    printf("here\n");
 
     uintptr_t entry = naive_uload(pcb, filename, NULL);
     pcb->cp = ucontext(NULL, (Area){pcb->stack, &(pcb->stack[STACK_SIZE])}, (void *)entry);
