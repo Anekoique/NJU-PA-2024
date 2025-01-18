944a71084250eb166af65c0ba6b11c533c13d1b4
tracer-ics2024 <tracer@njuics.org>
2025-01-18 19:47:00 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:47:00 up 3 days, 2 min,  1 user,  load average: 1.32, 1.61, 1.37

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -39,6 +39,7 @@ bool vme_init(void *(*pgalloc_f)(int), void (*pgfree_f)(void *))
         for (; va < segments[i].end; va += PGSIZE)
         {
             map(&kas, va, va, 0);
+            printf("%p\n", va);
         }
     }
 
