a3bbd4250e717a7267cff497084b9bca5f7e6adb
tracer-ics2024 <tracer@njuics.org>
2025-01-19 15:47:09 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:47:09 up 3 days, 20:03,  1 user,  load average: 1.61, 1.38, 1.48

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,7 +25,7 @@ int gettimeofday(struct _timeval *tv, intptr_t tz)
 
 size_t serial_write(const void *buf, size_t offset, size_t len)
 {
-    yield();
+    //yield();
     int num = 0;
     for (int i = 0; i < len && *(uint8_t *)buf != '\0'; i++)
     {
@@ -38,7 +38,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len)
 
 size_t events_read(void *buf, size_t offset, size_t len)
 {
-    yield();
+    //yield();
     AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
     if (ev.keycode == AM_KEY_NONE) return 0;
     int writed = 0;
@@ -60,7 +60,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len)
 
 size_t fb_write(const void *buf, size_t offset, size_t len)
 {
-    yield();
+    //yield();
     uint32_t *buffer = (uint32_t *)buf;
     int x = offset / sizeof(uint32_t) % 400;
     int y = offset / sizeof(uint32_t) / 400;
