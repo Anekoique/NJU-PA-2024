ab230c5e1bd471d0cf5a84ffc803f3cc2d5d79fa
tracer-ics2024 <tracer@njuics.org>
2025-01-19 17:39:09 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:39:09 up 3 days, 21:55,  1 user,  load average: 3.96, 1.48, 0.77

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -26,7 +26,6 @@ int isa_mmu_check(vaddr_t vaddr, int len, int type)
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type)
 {
-    printf("vaddr : %x\n", vaddr);
     uintptr_t offset = (uintptr_t)vaddr & 0xfff;
     uintptr_t vpn[2];
     vpn[0] = ((uintptr_t)vaddr >> 12) & 0x3ff;
@@ -34,11 +33,9 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type)
 
     uintptr_t pte = (uintptr_t)(((uintptr_t)(cpu.csr[4] << 12)) + vpn[1] * 4);
     uintptr_t pt = (paddr_read(pte, 4, 0) & 0xfffffc00) << 2;
-    printf("here\n");
     uintptr_t leaf_pte = (uintptr_t )(pt + vpn[0] * 4);
     uintptr_t pa = ((paddr_read(leaf_pte, 4, 0) & 0xfffffc00) << 2) + offset;
     assert(vaddr == pa);
-    printf("here\n");
     return pa;
 }
 
