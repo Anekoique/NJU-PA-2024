947af771698afd999e3d40da01d65dfde7a4197d
tracer-ics2024 <tracer@njuics.org>
2025-01-19 15:02:03 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:02:03 up 3 days, 19:17,  1 user,  load average: 2.20, 1.00, 1.23

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -32,8 +32,8 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type)
     vpn[0] = ((uintptr_t)vaddr >> 12) & 0x3ff;
     vpn[1] = ((uintptr_t)vaddr >> 22) & 0x3ff;
 
-    printf("here\n");
     uintptr_t *pte = (uintptr_t *)(((uintptr_t)cpu.csr[4] << 12) + vpn[1] * 4);
+    printf("here\n");
     uintptr_t pt = (*pte & 0xfffffc00) << 2;
     uintptr_t *leaf_pte = (uintptr_t *)(pt + vpn[0] * 4);
     uintptr_t pa = ((*leaf_pte & 0xfffffc00) << 2) + offset;
