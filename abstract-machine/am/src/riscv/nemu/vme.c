1d7e6f1e15b32ace60b2c73b6cd8d2878159a80e
tracer-ics2024 <tracer@njuics.org>
2025-01-19 15:34:38 +0800

    >  compile NEMU ctolu01@gmail.com ctolu Linux stoair-ASUS-TUF-Gaming-F15-FX507VU-FX507VU 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:34:38 up 3 days, 19:50,  1 user,  load average: 0.60, 1.21, 1.22

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -98,12 +98,11 @@ void map(AddrSpace *as, void *va, void *pa, int prot)
     *leaf_pte = (((uintptr_t)pa >> 12) << 10) | 0x3ff;
     if ((uintptr_t)va == 0x80001000) 
     {
-        printf("old : %p\n", *pte);
+        printf("old : %p\n", pte);
         printf("old : %p\n", vpn[0]);
         printf("old : %p\n", vpn[1]);
         printf("old : %p\n", (uintptr_t)as->ptr);
     }
-    printf("%x\n", (void *)0x80001978);
     assert((uintptr_t)va == (((*leaf_pte & 0xfffffc00) << 2) + offset));
 }
 
